58052c81fe55f889a04c1668e54e97aba7266dda
ElGutiar <el12guitar@gmail.com>
2021-01-24 16:50:30 +0300

    slider is ready

--- src/index.js
+++ src/index.js
@@ -23,3 +23,67 @@ hamburger.addEventListener('click', function () {
     });
   }
 });
+
+// Slider
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slider__item');
+const dotsContainer = document.querySelector('.slider__dots'); // !!!
+const btnLeft = document.querySelector('.slider__arrow--left');
+const btnRight = document.querySelector('.slider__arrow--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotsContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+const activeDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach((dot) => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${200 * (i - slide)}%)`)
+  );
+};
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+
+// Init
+createDots();
+activeDot(0);
+goToSlide(0);
+
+//Handles
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
